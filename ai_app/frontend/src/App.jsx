e5e7a0efb220ffdb3704a6c3cf1914cf332717f7
Ample1827 <Adalromero99@gmail.com>
2025-06-11 14:24:09 -0600

    Add imports for layout, challenge, history, and authentication components in App.jsx

--- ai_app/frontend/src/App.jsx
+++ ai_app/frontend/src/App.jsx
@@ -1,6 +1,11 @@
 import { useState } from 'react'
 import {ClerkProivderWithRoutes} from './auth/ClerkProviderWithRoutes.jsx'
 import {Routes, Route} from 'react-router-dom'
+import {layout} from './layout/layout.jsx'
+import { ChallengeGeneratr } from './challenge/ChallengeGeneratr.jsx'
+import { HistoryPanel } from './history/HistoryPanel.jsx'
+import { AuthenticationPage } from './auth/AuthenticationPage.jsx'
+
 import './App.css'
 
 function App() {
