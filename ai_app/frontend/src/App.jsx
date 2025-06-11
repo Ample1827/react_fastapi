ed630ff3440569dbaeedfd7d8756a533e17a4aff
Ample1827 <Adalromero99@gmail.com>
2025-06-11 14:22:39 -0600

    Add initial structure for authentication and challenge components

--- ai_app/frontend/src/App.jsx
+++ ai_app/frontend/src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
+import {ClerkProivderWithRoutes} from './auth/ClerkProviderWithRoutes.jsx'
+import {Routes, Route} from 'react-router-dom'
 import './App.css'
 
 function App() {
-  <> </>
+  return <ClerkProivderWithRoutes>
+      <Routes>
+        
+      </Routes>
+    </ClerkProivderWithRoutes>
 }
 
 export default App
